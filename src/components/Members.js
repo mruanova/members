7389f90d6bba45024988a6c5c8ece6b711f3af6c
Mauricio Ruanova-Hurtado <mruanova@gmail.com>
2020-05-11 17:37:10 -0500

    upgrade

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { Component } from 'react';
 import './Members.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Members extends Component {
   constructor(props) {
     super(props);
-    fetch(this.props.url).then(response => {return response.json();}).then(members => {
+    fetch(this.props.url).then(response => { return response.json(); }).then(members => {
       let output = '';
-      members.data.forEach(function(member){
+      members.data.forEach(function (member) {
         output += `<div class="member">
         <a href="${member.html_url}" target="_blank">
         <img src='${member.avatar_url}' alt='avatar' class='avatar'>
@@ -18,7 +17,7 @@ class Members extends Component {
       });
 
       document.getElementById('root').innerHTML = output;
-    }).catch(err => {console.log("error", err);});
+    }).catch(err => { console.log("error", err); });
   }
   render() {
     return null;
